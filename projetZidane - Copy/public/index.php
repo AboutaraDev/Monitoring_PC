42cc7626ca8e30a4ceaea4d87739d81a82ca2785
AboutaraDev <101973741+AboutaraDev@users.noreply.github.com>
2023-05-03 06:01:37 +0100

    Update index.php

--- projetZidane - Copy/public/index.php
+++ projetZidane - Copy/public/index.php
@@ -1,27 +1,4 @@
 
-<!--
-<! DOCTYPE html>    
-<html lang="en">    
-<head>    
-  <title> header Example </title>    
-  <meta charset="utf-8">    
-  <meta name="viewport" content="width=device-width, initial-scale=1">    
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">    
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>    
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"> </script>    
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"> </script>    
-   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-   <link rel = "stylesheet"   
-         href = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"  
-         integrity = "sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"  crossorigin = "anonymous">    
-   <link rel="stylesheet" href="header.css">
-   <link rel="stylesheet" href="footer.css">
-   
-
-</head>    
-@import url('https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&subset=devanagari,latin-ext'); 
-<body>
--->
 <?php
 
 require_once __DIR__ . '/../src/boostrap.php';
